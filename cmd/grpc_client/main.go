d292ea7c18b0554782afc282ad00bf9126955619
roman.fomindev <roman.fomindev@onlinesup.com>
2023-11-16 21:15:31 +0300

    fix

--- cmd/grpc_client/main.go
+++ cmd/grpc_client/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"context"
 	"flag"
-	"github.com/romanfomindev/microservices-chat-server/internal/client/grpc/auth"
+	"fmt"
 	"log"
+
+	"github.com/romanfomindev/microservices-chat-server/internal/client/grpc/auth"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 )
 
 var accessToken = flag.String("a", "", "access token")
@@ -14,9 +19,24 @@ func main() {
 
 	ctx := context.Background()
 
-	if auth.CheckAccess(ctx, "/api/v1/send_message", *accessToken) {
-		log.Fatal("Access granted")
-	} else {
+	md := metadata.New(map[string]string{"Authorization": "Bearer " + *accessToken})
+	ctx = metadata.NewOutgoingContext(ctx, md)
+
+	conn, err := grpc.Dial(
+		fmt.Sprintf(":%d", auth.ServicePort),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Printf("failed to dial GRPC client: %v", err)
+		return
+	}
+
+	client := auth.NewAuth(conn)
+
+	err = client.CheckAccess(ctx, "/api/v1/send_message")
+	if err != nil {
 		log.Fatal("Access denied")
 	}
+
+	log.Fatal("Access granted")
 }
